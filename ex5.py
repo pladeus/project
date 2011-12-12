905495a71efe41ec25c49c804ab91531ffc270e4
Jonas Steinvall <jonas.steinvall@gmail.com>
2011-12-12 19:37:27 +0100

    second commit

---
+++ ex5.py
@@ -0,0 +1,22 @@
+x="There are %d types of people." % 10
+binary="binary"
+do_not="don't"
+y="Those who know %s and those who %s." % (binary, do_not)
+
+print x
+print y
+
+print "I said: %r." % x
+print "I also said: '%s'." % y
+
+print "." * 10
+
+hilarious = False
+joke_evolution = "Isn't that joke so funny?! %f"
+
+print joke_evolution % hilarious
+
+w="This is the left side of..."
+e="a string with a right side."
+
+print w + e
